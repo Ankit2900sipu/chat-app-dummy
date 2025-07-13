5e2a5b317c81383ffba735cbad13befb0a36257f
Ankit2900sipu <ankit3626.sipu@gmail.com>
2025-07-14 01:50:54 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import ProfilePage from './pages/ProfilePage'
 
 function App() {
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain" >
+    <div className="bg-[url('/bgImage.svg')] bg-contain" >
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
